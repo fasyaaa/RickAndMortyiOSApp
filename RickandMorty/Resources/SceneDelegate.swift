a96d70f11244bce3b47498c38dfe7b71cee7afab
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-08-08 17:38:58 +0700

    plus keterangan

--- RickandMorty/Resources/SceneDelegate.swift
+++ RickandMorty/Resources/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        //untuk mengarahkan screen pertaman kali buka apk
         let vc = RMTabBarController()
         
         let window = UIWindow(windowScene: windowScene)
